5e12ed7cce6ffe98ccab63bbffc46971ae05fde9
SamSteele01 <ssteele017@gmail.com>
2019-02-20 12:50:02 -0500

    get thoughts out about flags and symbols

---
+++ api.js
@@ -0,0 +1,82 @@
+/* Bang */
+
+|or // used from here on as "or"
+/* explanation | title */
+//comment
+
+/* ! */
+!command // split[:1] everything after is input
+//
+
+
+@param
+alias @ name [@version]
+#id | tag
+##hash
+->#toHash
+#:isNumber
+-#toNumber
+-^#toBigNumber
+$value
+$$dollar | fiat
+%set | modify | filter | divide
+^atLeast | increases | set | change | control
+&and | weight | gas
+// gasLimit?
+&$gasPrice
+-flag // letter
+--flag // word
+.methodOf
+ . here
+
+type.method
+,seperate | list | split
+ all of this is a string // anything adjecent/following to whitespace
+this_is_all_one_word
+
+key:value
+"quote | exact "
+-"exact | to string "
+'option '
+*multiply
+-*marker
+()execute
+(arg) // input
+(arg, arg, arg) // CSV to array
+_private | join
+=assign
+==equals
+!=not equals
+
+|=pipe
++to | add
++=increment
+-=decrement
+?get
+// who what when where why
+/?question
+from->to | going | convert
+<-back | left | remainder
+#%undo
+/split
+-/divide
+==even
+||logical or
+|/even split
+/#break | stop
+\escape
+-\goBack
+-:variable | scoped variable
+-|constant
+-<variable
+>-funnel | filter
+>=greater than
+<=assign | less than
+=>function | process
+// () => {}
+convert this => to this
+take these => apply this process
+0x
+
+can split on symbol to get [0, 1]
+symbol -> key, string -> value
